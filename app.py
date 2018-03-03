69ed5e6407737055911582dba47693333d2930f8
tianyizheng <tianyizheng93@gmail.com>
2018-03-03 15:26:48 -0500

    angular and workers

--- app.py
+++ app.py
@@ -8,72 +8,68 @@ import operator
 import re
 # natural language
 import nltk
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 # coutns occurances
 from collections import Counter
 # to scrape html and xml
 from bs4 import BeautifulSoup
+from rq import Queue
+from rq.job import Job
+from worker import conn
+
+################Configurations##############################
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+# sets up a Redis connection and init queue
+q = Queue(connection=conn)
 
+#############################################################
 from models import Result
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-  errors = []
-  results = {}
-  r = None
-  if request.method == "POST":
+def count_and_save_words(url):
+
+    errors = []
+
     try:
-      # handle GET and POST requests, grab value
-      # corresponding to url key
-      # use request.form.get('url') is key might not exist
-      url = request.form['url']
-      # to send GET requests to grab the content from the URL
-      r = requests.get(url)
+        r = requests.get(url)
     except:
-      errors.append("Not a valid URL request")
-    if r:
-      raw = BeautifulSoup(r.text, 'html.parser').get_text()
-      # set path
-      nltk.data.path.append('./nltk_data/')
-      # list of strings of tokenized words
-      tokens = nltk.word_tokenize(raw)
-      stops = nltk.corpus.stopwords.words('english')
-      # a wrapper around tokens to Text class
-      # use it later for regex search
-      text = nltk.Text(tokens)
-      # remove puntuations. compiles into regex object
-      # faster to compile since this will be cached
-      nonPunct = re.compile('[A-Za-z]+')
-      punct = re.compile('[\W]+|\d')
-      # store all words matched with regex into list
-      # only matches the beginnign of string
-      raw_words = [w for w in text if nonPunct.match(w) and not punct.search(w)]
-      # get raw word counts
-      raw_word_count = Counter(raw_words)
-      # get rid of all stop words, which are in lower case only
-      no_stop_words = [w for w in raw_words if w.lower() not in stops]
-      # get new count. 
-      no_stop_words_count = Counter(no_stop_words)
-      # store into results
-      # sorted -> build new sorted list from iterable
-      results = sorted(
-        # returns a list of tuples
-        no_stop_words_count.items(),
-        # this will get the second position element from the tuple
-        # multiple item extraction
-        # love this method
-        key=operator.itemgetter(1),
-        reverse=True
-      )[:10]
-      # always be careful when querrying with database
-      try:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    raw = BeautifulSoup(r.text, 'html.parser').get_text()
+    # set path
+    nltk.data.path.append('./nltk_data/')
+    # list of strings of tokenized words
+    tokens = nltk.word_tokenize(raw)
+    stops = nltk.corpus.stopwords.words('english')
+    # a wrapper around tokens to Text class
+    # use it later for regex search
+    text = nltk.Text(tokens)
+    # remove puntuations. compiles into regex object
+    # faster to compile since this will be cached
+    nonPunct = re.compile('[A-Za-z]+')
+    punct = re.compile('[\W]+|\d')
+    # store all words matched with regex into list
+    # only matches the beginnign of string
+    raw_words = [w for w in text if nonPunct.match(w) and not punct.search(w)]
+    # get raw word counts
+    raw_word_count = Counter(raw_words)
+    # get rid of all stop words, which are in lower case only
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    # get new count. 
+    no_stop_words_count = Counter(no_stop_words)
+    # always be careful when querrying with database
+
+    # save the results
+    try:
         result = Result(
             url=url,
             result_all=raw_word_count,
@@ -81,14 +77,42 @@ def index():
         )
         db.session.add(result)
         db.session.commit()
-      except:
+        return result.id
+    except:
         errors.append("Unable to add item to database.")
-  return render_template('index.html', errors=errors, results=results)
+        return {"error": errors}
 
-@app.route('/<name>')
-def hello_name(name):
-  return "Hello {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  return render_template('index.html')
 
+@app.route('/start', methods=['POST'])
+def get_counts():
+  data = json.loads(request.data.decode())
+  url = data['url']
+  # to send GET requests to grab the content from the URL
+  if 'http://' not in url[:7] and 'https://' not in url[:8]:
+      url = 'http://' + url
+  job = q.enqueue_call(
+    func=count_and_save_words, args=(url,), result_ttl=50
+    )
+  return (job.get_id())
+  
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+    print(job.is_finished)
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
 
 if __name__ == '__main__':
   app.run()
